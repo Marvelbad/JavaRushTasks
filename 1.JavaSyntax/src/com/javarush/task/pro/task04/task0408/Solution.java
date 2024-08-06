967754bda0da9eb02b55a174aa3d8fc81372fee5
Marvelbad <badra.aqua@gmail.com>
2024-08-06 17:36:52 +0300

    Почти сам.

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0408/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0408/Solution.java
@@ -8,7 +8,24 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        Scanner scanner = new Scanner(System.in);
+        int maxNumber = Integer.MIN_VALUE;
+        boolean isEven = false;
 
+        while (scanner.hasNextInt()) {
+            int curNum = scanner.nextInt();
+            if (curNum % 2 == 0) {
+                isEven = true;
+                if (curNum > maxNumber) {
+                    maxNumber = curNum;
+                }
+            }
+        }
+
+        if (isEven) {
+            System.out.println(maxNumber);
+        }   else {
+            System.out.println(Integer.MIN_VALUE);
+        }
     }
 }
