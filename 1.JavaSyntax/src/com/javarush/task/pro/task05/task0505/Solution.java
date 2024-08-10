4d4cbbfcc3080916dea79169edc034be0b154e82
Marvelbad <badra.aqua@gmail.com>
2024-08-10 23:16:07 +0300

    Вернуться сюда позже!!! Разобрать код по кусочкам

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0505/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0505/Solution.java
@@ -11,22 +11,18 @@ Reverse
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(reader.readLine());
-        if (N > 0) {
-            int[] numbers = new int[N];
-            for (int i = 0; i < N; i++) {
-                numbers[i] = Integer.parseInt(reader.readLine());
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int[] arrayNum = new int[N];
+        for (int i = 0; i < N; i++) {
+            arrayNum[i] = scanner.nextInt();
+        }
+        int min = arrayNum[0];
+        for (int i = 0; i < N; i++) {
+            if (arrayNum[i] < min) {
+                min = arrayNum[i];
             }
-            if (N % 2 == 1) {
-                for (int i = 0; i < N; i++) {
-                    System.out.println(numbers[i]);
-                }
-            } else {
-                for (int i = N - 1; i >= 0; i--) {
-                    System.out.println(numbers[i]);
-                }
-            }
+            System.out.println(min);
         }
     }
 }
--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0506/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0506/Solution.java
@@ -7,24 +7,17 @@ import java.util.Scanner;
 */
 
 public class Solution {
-    public static int[] array;
+    //public static int[] array;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n = Integer.parseInt(scanner.nextLine());
+        String[] array = new String[10];
 
-        array = new int[n];
-        for (int i = 0; i < n; i++) {
-            array[i] = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i < 10; i++) {
+            array[i] = scanner.nextLine();
         }
-
-        int min = array[0];
-        for (int i = 1; i < array.length; i++) {
-            int number = array[i];
-            if (number < min) {
-                min = number;
-            }
+        for (int i = 9; i >= 0; i--) {
+            System.out.println(array[i]);
         }
-        System.out.println(min);
     }
 }
