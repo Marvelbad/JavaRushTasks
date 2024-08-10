3b776389b5245012fe7778d6dd541823b8d8d895
Marvelbad <badra.aqua@gmail.com>
2024-08-10 16:04:51 +0300

    Вернуться сюда позже!!! Разобрать код по кусочкам

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0508/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0508/Solution.java
@@ -11,22 +11,24 @@ public class Solution {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         strings = new String[6];
         for (int i = 0; i < 6; i++) {
             strings[i] = scanner.nextLine();
         }
+
         for (int i = 0; i < strings.length; i++) {
-            if (strings[i] == null) {
-                continue;
-            }
+            String currentString = strings[i];
             for (int j = i + 1; j < strings.length; j++) {
-                if (strings[j] != null && strings[i].equals(strings[j])) {
-                    strings[i] = null;
+                if (currentString == null) {
+                    break;
+                }
+                if (currentString.equals(strings[j])) {
                     strings[j] = null;
+                    strings[i] = null;
                 }
             }
         }
-
         for (int i = 0; i < strings.length; i++) {
             System.out.print(strings[i] + ", ");
         }
