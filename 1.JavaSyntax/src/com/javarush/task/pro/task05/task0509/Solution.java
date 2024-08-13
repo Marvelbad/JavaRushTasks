5365d244f0ab6159231330e31428f3ee810121ab
Marvelbad <badra.aqua@gmail.com>
2024-08-13 21:27:09 +0300

    Разобрался

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0509/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.pro.task05.task0509;
+
+/* 
+Таблица умножения
+*/
+
+public class Solution {
+
+    public static int[][] MULTIPLICATION_TABLE;
+
+    public static void main(String[] args) {
+       int start = 0;
+       int end = 50;
+       for (int i = start; i < end; i++) {
+           start++;
+           System.out.println(start);
+       }
+    }
+}
