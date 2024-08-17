aa63c235d120985926a65f0b135b6183bdd323e6
Marvelbad <badra.aqua@gmail.com>
2024-08-17 12:27:35 +0300

    Done

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0516/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0516/Solution.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 
 public class Solution {
 
-    public static int[] array = new int[21];
+    public static int[] array = new int[20];
     public static int valueStart = 10;
     public static int valueEnd = 13;
 
@@ -17,10 +17,9 @@ public class Solution {
             Arrays.fill(array, 0, array.length / 2, valueStart);
             Arrays.fill(array, array.length / 2, array.length, valueEnd);
         } else {
-            Arrays.fill(array, 0, array.length / 2 + 1, valueStart);
-            Arrays.fill(array,  array.length / 2 + 1, array.length, valueEnd);
+            Arrays.fill(array,0, array.length / 2 + 1, valueStart);
+            Arrays.fill(array, array.length / 2 + 1, array.length, valueEnd);
         }
-
         System.out.println(Arrays.toString(array));
     }
 }
