183ee7197451d01c9b7cf9fa5cdaa7bb30b5596a
Marvelbad <badra.aqua@gmail.com>
2024-08-21 19:15:58 +0300

    Done

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,53 @@
+package com.javarush.task.pro.task06.task0606;
+
+/* 
+Универсальный солдат
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        universalMethod();
+        universalMethod(10);
+        universalMethod("Hello");
+        universalMethod(10.4);
+        universalMethod(true);
+        universalMethod(12.3);
+        universalMethod(10.243);
+        universalMethod(26, "Hola");
+        universalMethod(30, 20.6);
+        universalMethod(10,34.8);
+    }
+
+    public static void universalMethod(){
+
+    }
+
+    public static void universalMethod(int a) {
+
+    }
+    public static void universalMethod(String str) {
+
+    }
+    public static void universalMethod(double d) {
+
+    }
+    public static void universalMethod(boolean flag) {
+
+    }
+    public static void universalMethod(char c) {
+
+    }
+    public static void universalMethod(long l) {
+
+    }
+    public static void universalMethod(int a, String c) {
+
+    }
+    public static void universalMethod(int a, double d) {
+
+    }
+    public static void universalMethod(int a, char c) {
+
+    }
+}
