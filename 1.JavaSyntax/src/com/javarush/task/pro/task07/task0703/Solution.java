1ec809b2309446607c70eb1001846a4d5694e2e9
Marvelbad <badra.aqua@gmail.com>
2024-08-25 20:30:13 +0300

    Done

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0703/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task07.task0703;
+
+/* 
+Минимальное целое
+*/
+
+public class Solution {
+    int num1 = Integer.MIN_VALUE;
+    short num2 = Short.MIN_VALUE;
+    long num3 = Long.MIN_VALUE;
+    byte num4 = Byte.MIN_VALUE;
+}
