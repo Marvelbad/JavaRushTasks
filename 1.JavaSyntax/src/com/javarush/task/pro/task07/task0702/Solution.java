77b365cfd2097f7fd250801863e8fed457e588a8
Marvelbad <badra.aqua@gmail.com>
2024-08-25 20:27:42 +0300

    Done

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0702/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task07.task0702;
+
+/* 
+Максимальное целое
+*/
+
+public class Solution {
+    int num = Integer.MAX_VALUE;
+    byte num2 = Byte.MAX_VALUE;
+    short num3 = Short.MAX_VALUE;
+    long num4 = Long.MAX_VALUE;
+}
