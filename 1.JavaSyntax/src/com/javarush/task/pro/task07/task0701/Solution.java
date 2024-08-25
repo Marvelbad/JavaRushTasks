e29e0bb2ec44c55fa4aa1c272e48e1ed37d3d074
Marvelbad <badra.aqua@gmail.com>
2024-08-25 20:11:12 +0300

    Done

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0701/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task07.task0701;
+
+/* 
+Все возможные типы
+*/
+
+public class Solution {
+    int num;
+    byte num2;
+    short num3;
+    long num4;
+    float num5;
+    double num6;
+    boolean isTrue;
+    char num8;
+}
