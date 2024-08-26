8af574cea2d7cf585a7ed39053bf53407baa9c3c
Marvelbad <badra.aqua@gmail.com>
2024-08-26 19:37:00 +0300

    Done

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0712/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.pro.task07.task0712;
+
+/* 
+Первые объекты
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        String str = new String();
+        String str2 = new String();
+        String str3 = new String();
+        String str4 = new String();
+        String str5 = new String();
+        String str6 = new String();
+        String str7 = new String();
+        String str8 = new String();
+        String str9 = new String();
+        String str0 = new String();
+
+        int[] array = new int[5];
+        int[] array2 = new int[5];
+        int[] array3 = new int[5];
+        int[] array4 = new int[5];
+        int[] array5 = new int[5];
+
+        Scanner scan = new Scanner(System.in);
+        Scanner scan2 = new Scanner(System.in);
+    }
+}
