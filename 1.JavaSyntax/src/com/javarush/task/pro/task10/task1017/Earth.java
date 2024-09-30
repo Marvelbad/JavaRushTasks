8560e1b0a8dca72935144507f37bc7790533a4d8
Marvelbad <badra.aqua@gmail.com>
2024-09-30 15:29:08 +0300

    Done

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1017/Earth.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task10.task1017;
+
+/* 
+Создание материков
+*/
+
+public class Earth {
+    public static void main(String[] args) {
+        Africa africa = new Africa(234543);
+        Antarctica antarctica = new Antarctica(345765);
+        Australia australia = new Australia(342516);
+        Eurasia eurasia = new Eurasia(354637);
+        NorthAmerica northAmerica = new NorthAmerica(746574);
+        SouthAmerica southAmerica = new SouthAmerica(4635647);
+    }
+}
