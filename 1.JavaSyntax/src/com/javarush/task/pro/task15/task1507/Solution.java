3dd6182fe0a4047e4e24cfa44cdf036e494a2686
Marvelbad <badra.aqua@gmail.com>
2024-11-06 20:09:34 +0300

    add Solution Java Syntax task 1507

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1507/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.pro.task15.task1507;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Scanner;
+
+/* 
+Пропускаем не всех
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        String fileName = scanner.nextLine();
+        Path path = Paths.get(fileName);
+
+        List<String> lines = Files.readAllLines(path);
+        for (int i = 0; i < lines.size(); i += 2) {
+            System.out.println(lines.get(i));
+        }
+    }
+}
+
