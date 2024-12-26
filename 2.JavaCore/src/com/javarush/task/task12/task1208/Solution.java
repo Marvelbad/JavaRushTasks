3e0f7c6b14ef0db2b41279ef1449d64b07453008
Marvelbad <badra.aqua@gmail.com>
2024-12-26 19:45:33 +0300

    add Solution JavaCore task 1208

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print(String b){
+
+    }
+
+    public static void print(int a) {
+
+    }
+
+    public static void print(int a, String b) {
+
+    }
+
+    public static void print(String a, int b) {
+
+    }
+
+    public static void print(double c) {
+
+    }
+}
