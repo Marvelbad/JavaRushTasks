95e8f73e291938e952b780fa94ea97a5c9408047
Marvelbad <badra.aqua@gmail.com>
2025-01-05 13:31:37 +0300

    add Solution JavaCore task 1319

--- 2.JavaCore/src/com/javarush/task/task13/task1319/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task13/task1319/Solution.java
@@ -1,16 +1,27 @@
 package com.javarush.task.task13.task1319;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.InputStreamReader;
+import java.io.*;
 
 /* 
 Писатель в файл с консоли
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            String sourceFile = reader.readLine();
 
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(sourceFile))) {
+                while (true) {
+                    String str = reader.readLine();
+                    writer.write(str);
+                    writer.newLine();
+
+                    if (str.equalsIgnoreCase("exit")) {
+                        break;
+                    }
+                }
+            }
+        }
     }
 }
