e522ef585863b24d8ba7a5a48ee6fefaf40e1ab7
Marvelbad <badra.aqua@gmail.com>
2025-01-12 12:36:06 +0300

    add Solution JavaCore task 1411

--- 2.JavaCore/src/com/javarush/task/task14/task1411/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task14/task1411/Solution.java
@@ -10,32 +10,43 @@ User, Loser, Coder and Proger
 public class Solution {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
         Person person = null;
-
-
+        //read values from keyboard here
+        String key;
         while (true) {
-            String key = reader.readLine();
-            if (key.equalsIgnoreCase("user")) {
-                doWork(new Person.User());
-            } else if (key.equalsIgnoreCase("loser")) {
-                doWork(new Person.Loser());
-            } else if (key.equalsIgnoreCase("coder")) {
-                doWork(new Person.Coder());
-            } else if (key.equalsIgnoreCase("proger")) {
-                doWork(new Person.Proger());
+            key = reader.readLine();
+            boolean u = key.equals("user") || key.equals("loser") || key.equals("coder") || key.equals("proger");
+            if (!u) break;
+
+            switch (key) {
+                case "user":
+                    person = new Person.User();
+                    break;
+                case "loser":
+                    person = new Person.Loser();
+                    break;
+                case "coder":
+                    person = new Person.Coder();
+                    break;
+                case "proger":
+                    person = new Person.Proger();
+                    break;
             }
             doWork(person);
-
         }
     }
 
     public static void doWork(Person person) {
         if (person instanceof Person.User) {
-            ((Person.User) person).live();
+            Person.User user = (Person.User) person;
+            user.live();
         } else if (person instanceof Person.Loser) {
-            ((Person.Loser) person).doNothing();
+            Person.Loser loser = (Person.Loser) person;
+            loser.doNothing();
         } else if (person instanceof Person.Coder) {
-            ((Person.Coder) person).writeCode();
+            Person.Coder coder = (Person.Coder) person;
+            coder.writeCode();
         } else if (person instanceof Person.Proger) {
             Person.Proger proger = (Person.Proger) person;
             proger.enjoy();
