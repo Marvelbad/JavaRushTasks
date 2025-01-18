2cc88d75169756313d9d4f197786eceddb5a044d
Marvelbad <badra.aqua@gmail.com>
2025-01-18 15:50:48 +0300

    add Solution JavaCore task 1519 (in progress)

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1519/Solution.java
@@ -0,0 +1,49 @@
+package com.javarush.task.task15.task1519;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 
+Разные методы для разных типов
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            String str = reader.readLine();
+            if (str.equals("exit")) {
+                return;
+            }
+
+            if (str.contains(".")) {
+                print(Double.parseDouble(str));
+            } else {
+                int num = (Integer.parseInt(str));
+                if (num > 0 && num < 128) {
+                    print((short) num);
+                } else {
+                    print(num);
+                } //via regular expression
+            }
+        }
+    }
+
+    public static void print(Double value) {
+        System.out.println("Это тип Double, значение " + value);
+    }
+
+    public static void print(String value) {
+        System.out.println("Это тип String, значение " + value);
+    }
+
+    public static void print(short value) {
+        System.out.println("Это тип short, значение " + value);
+    }
+
+    public static void print(Integer value) {
+        System.out.println("Это тип Integer, значение " + value);
+    }
+}
