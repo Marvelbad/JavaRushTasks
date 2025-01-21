66acc25970ba4569913ff56c4188234ef0a8b048
Marvelbad <badra.aqua@gmail.com>
2025-01-21 15:22:15 +0300

    add Solution JavaCore task 1527

--- 2.JavaCore/src/com/javarush/task/task15/task1527/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task15/task1527/Solution.java
@@ -14,16 +14,33 @@ public class Solution {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String url = reader.readLine();
 
-        String string = url.substring(url.lastIndexOf("?") +1);
+        String string = url.substring(url.lastIndexOf("?") + 1);
         String[] arguments = string.split("&");
+
+        String objValue = null;
 
         for (String argument : arguments) {
             if (argument.contains("=")) {
                 int index = argument.indexOf("=");
                 String argumentName = argument.substring(0, index);
+                String value = argument.substring(index + 1);
+                if (argumentName.equals("obj")) {
+                    objValue = value;
+                }
                 System.out.print(argumentName + " ");
             } else {
                 System.out.print(argument + " ");
+            }
+        }
+
+        System.out.println();
+
+        if (objValue != null) {
+            try {
+                double doubleValue = Double.parseDouble(objValue);
+                alert(doubleValue);
+            } catch (NumberFormatException e) {
+                alert(objValue);
             }
         }
     }
