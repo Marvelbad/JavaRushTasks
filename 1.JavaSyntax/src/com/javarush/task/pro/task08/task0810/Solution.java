d81bf7d867b6d81168397a59dd71d42a80789f51
Marvelbad <badra.aqua@gmail.com>
2025-01-27 12:10:27 +0300

    add Solution JavaSyntax task 0810

--- 1.JavaSyntax/src/com/javarush/task/pro/task08/task0810/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0810/Solution.java
@@ -10,11 +10,13 @@ public class Solution {
 
     public static void setTimer(int days, int hours, int minutes, int seconds, int millis) throws InterruptedException {
         System.out.println("Таймер запущен!");
-        TimeUnit.DAYS.sleep(2);
-        TimeUnit.HOURS.sleep(2);
-        TimeUnit.MINUTES.sleep(2);
-        TimeUnit.SECONDS.sleep(2);
-        TimeUnit.MILLISECONDS.sleep(2);
+        TimeUnit.DAYS.sleep(days);
+        TimeUnit.HOURS.sleep(hours);
+        TimeUnit.MINUTES.sleep(minutes);
+        TimeUnit.SECONDS.sleep(seconds);
+        TimeUnit.MILLISECONDS.sleep(millis);
         System.out.println("♬ ♪ ♬♬♬♬ ♪♪♪♪");
     }
+
+    
 }
