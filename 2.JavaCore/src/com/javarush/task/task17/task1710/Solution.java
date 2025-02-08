d3e2513deea920b3d05bcb60433e1951f0f0c496
Marvelbad <badra.aqua@gmail.com>
2025-02-08 15:36:00 +0300

    add Solution JavaCore task 1710

--- 2.JavaCore/src/com/javarush/task/task17/task1710/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task17/task1710/Solution.java
@@ -22,6 +22,7 @@ public class Solution {
         Person person = null;
 
         SimpleDateFormat input = new SimpleDateFormat("dd/MM/yyyy");
+        SimpleDateFormat output = new SimpleDateFormat("dd-MMM-yyyy");
         if (args[0].equals("-c")) {
 
             if (args[2].equalsIgnoreCase("м")) {
@@ -34,7 +35,42 @@ public class Solution {
             allPeople.add(person);
 
             System.out.println(allPeople.indexOf(person));
-        }
+        } else if (args[0].equals("-r")) {
+            int id = Integer.parseInt(args[1]);
+            if (id >= 0 && id < allPeople.size()) {
+                person = allPeople.get(id);
+                String name = person.getName();
+                Sex sex = person.getSex();
+                String stringSex = null;
+                if (sex.equals(Sex.MALE)) {
+                    stringSex = "м";
+                } else if (sex.equals(Sex.FEMALE)) {
+                    stringSex = "ж";
+                }
+                Date birthDate = person.getBirthDate();
+                System.out.println(name + " " + stringSex + " " + output.format(birthDate));
+            }
+        } else if (args[0].equals("-u")) {
+            int id = Integer.parseInt(args[1]);
+            if (id >= 0 && id < allPeople.size()) {
+                person = allPeople.get(id);
+                person.setName(args[2]);
+                if (args[3].equalsIgnoreCase("м")) {
+                    person.setSex(Sex.MALE);
+                } else if (args[3].equalsIgnoreCase("ж")) {
+                    person.setSex(Sex.FEMALE);
+                }
+                person.setBirthDate(input.parse(args[4]));
+            }
+        } else if (args[0].equals("-d")) {
+            int id = Integer.parseInt(args[1]);
 
+            if (id >= 0 && id < allPeople.size()) {
+                person = allPeople.get(id);
+                person.setName(null);
+                person.setSex(null);
+                person.setBirthDate(null);
+            }
+        }
     }
 }
