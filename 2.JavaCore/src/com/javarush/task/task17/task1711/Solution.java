8334ac4397ee03a7fb3a81e688e94591ec450018
Marvelbad <badra.aqua@gmail.com>
2025-02-09 18:37:13 +0300

    add Solution JavaCore task 1711 first switch done

--- 2.JavaCore/src/com/javarush/task/task17/task1711/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task17/task1711/Solution.java
@@ -24,6 +24,27 @@ public class Solution {
         SimpleDateFormat input = new SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH);
         SimpleDateFormat output = new SimpleDateFormat("dd-MMM-yyyy", Locale.ENGLISH);
 
+        switch (args[0]) {
+            case "-c":
+                synchronized (allPeople) {
+                    for (int i = 1; i < args.length; i += 3) {
+                        try {
+                            String name = args[i];
+                            String sex = args[i + 1];
+                            Date bd = input.parse(args[i + 2]);
 
+                            if ("м".equalsIgnoreCase(sex)) {
+                                allPeople.add(Person.createMale(name, bd));
+                            } else if ("ж".equalsIgnoreCase(sex)) {
+                                allPeople.add(Person.createFemale(name, bd));
+                            }
+
+                            System.out.println(allPeople.size() - 1);
+                        } catch (Exception e) {
+                            System.out.println(e.getMessage());
+                        }
+                    }
+                }
+        }
     }
 }
