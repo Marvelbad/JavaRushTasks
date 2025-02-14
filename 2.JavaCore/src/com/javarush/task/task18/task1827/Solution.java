2e95709c640b0c3a4ac161858afc7e706279a3cb
Marvelbad <badra.aqua@gmail.com>
2025-02-14 16:25:22 +0300

    add Solution JavaCore task 1827

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1827/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task18.task1827;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+/* 
+Прайсы
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//        Path path = Paths.get(reader.readLine());
+//        List<String> strings = Files.readAllLines(path);
+//
+//
+//        Integer max = strings.stream()
+//                .map(str -> str.substring(0, 8).trim())
+//                .map(Integer::parseInt)
+//                .max(Comparator.naturalOrder()).get();
+        //Filereader
+        //filename in string expression
+    }
+}
