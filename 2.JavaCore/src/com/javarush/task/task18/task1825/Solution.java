fc6053b7e93f200631f44df704bb89811fab4356
Marvelbad <badra.aqua@gmail.com>
2025-02-14 11:11:07 +0300

    add Solution JavaCore task 1825

--- 2.JavaCore/src/com/javarush/task/task18/task1825/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1825/Solution.java
@@ -2,6 +2,10 @@ package com.javarush.task.task18.task1825;
 
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.StandardOpenOption;
 import java.util.*;
 
 /* 
@@ -12,45 +16,47 @@ public class Solution {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        /// ///////treeMap !!!!!!!!!!!!!
-
-        TreeSet<String> fileParts = new TreeSet<String>();
+        TreeMap<Integer, String> files = new TreeMap<>();
 
         while (true) {
-            try {
-                String input = reader.readLine();
-                if (input.equalsIgnoreCase("end")) {
-//                    System.out.println(fileParts);
-                    break;
-                }
-                fileParts.add(input);
-            } catch (Exception e) {
-                System.out.println("Error: " + e.getMessage());
+            String input = reader.readLine();
+            if (input.equalsIgnoreCase("end")) break;
+
+            String number = input.substring(input.lastIndexOf("t") + 1);
+            int index = Integer.parseInt(number);
+
+            if (!files.containsKey(index)) {
+                files.put(index, input);
             }
         }
 
-
-        String str = fileParts.first();
-        int index = str.lastIndexOf(".part");
-        if (index == -1) {
-            System.out.println("Warning: Skipping invalid file - " + str);
-            System.exit(1);
-        }
-        String result = str.substring(0, index);
-
-//        System.out.println(result);
-
-        try (FileOutputStream outputStream = new FileOutputStream(result)) {
-            for (String part : fileParts) {
-                byte[] buffer = new byte[1024];
-                try (FileInputStream inputStream = new FileInputStream(part)) {
-                    int bytesRead;
-                    while ((bytesRead = inputStream.read(buffer)) != -1) {
-                        outputStream.write(buffer, 0, bytesRead);
-                    }
+        for (String str : files.values()) {
+            String fileName = str.substring(0, str.lastIndexOf("."));
+            try (BufferedInputStream input = new BufferedInputStream(new FileInputStream(str));
+                 BufferedOutputStream output = new BufferedOutputStream(new FileOutputStream(fileName, true))) {
+                while (input.available() > 0) {
+                    output.write(input.read());
                 }
             }
         }
+
+        for (String value : files.values()) {
+            Files.write(
+                    Path.of(value.substring(0, value.lastIndexOf("."))),
+                    Files.readAllBytes(Path.of(value)),
+                    StandardOpenOption.APPEND
+            );
+
+        }
+
+//        files.values()
+//                .forEach(value -> {
+//                    try {
+//                        Files.write(Path.of(value.substring(0, value.lastIndexOf("."))), Files.readAllBytes(Path.of(value)), StandardOpenOption.APPEND);
+//                    } catch (IOException e) {
+//                        throw new RuntimeException(e);
+//                    }
+//                });
     }
 }
 
