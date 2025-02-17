5df9a2ffe1ddf40b198edf1b83bc773653545430
Marvelbad <badra.aqua@gmail.com>
2025-02-17 22:11:30 +0300

    add Solution JavaCore task 1827

--- 2.JavaCore/src/com/javarush/task/task18/task1827/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1827/Solution.java
@@ -14,14 +14,15 @@ import java.util.List;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader console = new BufferedReader(new InputStreamReader(System.in)); //Для считывания с консоли
 
-        String fileName = console.readLine();
-        try (BufferedReader reader = new BufferedReader(new FileReader(fileName));
-             BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true))) {
-            while (reader.ready()) {
-                String result = reader.readLine();
-                String subStr = result.substring(0, 8);
+        String fileName = console.readLine(); //Здесь считали адрес то что ввели в консоли
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName)); //Тут открыли поток для чтения этого файла по этому адресу.
+             BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true))) { //А тут для записи новых данных по тому же адресу.
+            while (reader.ready()) { //Здесь цикл чтобы, проверять, есть ли еще данные. То есть именно по строкам.
+                String fileInside = reader.readLine();
+                int id = Integer.parseInt(fileInside.substring(0, 8));
+
             }
 
         }
