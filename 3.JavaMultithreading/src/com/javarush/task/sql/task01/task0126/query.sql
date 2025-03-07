26ccbc4ddf957322c250f75ef1c84ea5b6af9d3e
Marvelbad <badra.aqua@gmail.com>
2025-03-07 21:35:17 +0300

    add Solution SQL 0126

--- 3.JavaMultithreading/src/com/javarush/task/sql/task01/task0126/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task01/task0126/query.sql
@@ -1,5 +1,5 @@
 -- Write your code here:
 SELECT brand, model, quantity, booked_quantity
 FROM car
-WHERE brand IN ('renault', 'opel', 'skoda')
+WHERE brand IN ('renault', 'opel', 'skoda', 'seat')
    OR (quantity - booked_quantity) < 10
