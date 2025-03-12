a0c4ec09d8bdada2e599784cadca12920eccf543
Marvelbad <badra.aqua@gmail.com>
2025-03-12 18:01:54 +0300

    add Solution SQL 0203

--- 3.JavaMultithreading/src/com/javarush/task/sql/task02/task0203/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task02/task0203/query.sql
@@ -1,7 +1,2 @@
 -- Write your code here:
-UPDATE numbers
-SET number1 = 1
-WHERE id = 1;
-
-SELECT IF(number1 < number2, 'yes', 'no') AS result
-FROM numbers
+SELECT IF(4 < 1, 'yes', 'no') AS result
