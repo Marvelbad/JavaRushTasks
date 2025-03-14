3dbc230f917476349c1889d506f770150422457c
Marvelbad <badra.aqua@gmail.com>
2025-03-14 21:00:32 +0300

    add Solution SQL 0344

--- 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0344/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0344/query.sql
@@ -1,8 +1,6 @@
 -- Write your code here:
-SELECT films.title
-    FROM films
-LEFT JOIN (
-    SELECT last_name
-        FROM film_directors AS director
-        WHERE last_name = 'Spielberg'
-    ) AS director ON films.director_id = director_id;
+SELECT title
+FROM films
+         LEFT JOIN (SELECT last_name, id
+                    FROM film_directors
+                    WHERE last_name = 'Spielberg') AS director ON director.id = films.director_id;
