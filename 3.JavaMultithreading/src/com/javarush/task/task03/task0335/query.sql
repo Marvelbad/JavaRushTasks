9726d342cc86f24efd16a36f18281ee1c3e299e4
Marvelbad <badra.aqua@gmail.com>
2025-03-14 21:14:04 +0300

    add Solution SQL 0335

--- 3.JavaMultithreading/src/com/javarush/task/task03/task0335/query.sql
+++
@@ -1,8 +0,0 @@
--- Write your code here:
-SELECT author.country AS author_country,
-       COUNT(book.book_id) AS book_count
-    FROM authors AS author
-JOIN books AS book ON author.author_id = book.author_id
-WHERE book.genre = 'novel'
-GROUP BY author.country
-HAVING COUNT(book.book_id) > 2;
