edca1615b0f5acb6e0afebcc0d3de46c185e95b1
Marvelbad <badra.aqua@gmail.com>
2025-03-14 21:06:53 +0300

    add Solution SQL 0345

--- 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0345/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0345/query.sql
@@ -1,7 +1,7 @@
 -- Write your code here:
 SELECT last_name
 FROM film_directors
-         LEFT JOIN (SELECT grossed
-         FROM films AS film
-         WHERE grossed > 100) AS film ON film_directors.id = film.director_id
-         WHERE film_directors.country = 'UK';
+         LEFT JOIN (SELECT grossed, director_id
+                    FROM films
+                    WHERE grossed > 100) AS film ON film_directors.id = film.director_id
+WHERE film_directors.country = 'UK';
