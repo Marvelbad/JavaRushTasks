a4308356f19a33e92db475e04a66f7e8a58194dd
Marvelbad <badra.aqua@gmail.com>
2025-03-14 21:13:09 +0300

    add Solution SQL 0322

--- 3.JavaMultithreading/src/com/javarush/task/task03/task0322/query.sql
+++
@@ -1,6 +0,0 @@
--- Write your code here:
-SELECT c.customer_id, c.email,
-       o.order_id, o.order_status
-    FROM customers AS c
-JOIN orders AS o ON c.customer_id = o.customer_id
-WHERE o.order_status = 'PAID' OR o.order_status = 'PROCESSING';
