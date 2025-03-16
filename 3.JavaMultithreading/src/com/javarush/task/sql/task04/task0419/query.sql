44c2efaafe1279709e28efef9c952501c3147722
Marvelbad <badra.aqua@gmail.com>
2025-03-16 20:23:23 +0300

    add Solution SQL 0419

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task04/task0419/query.sql
@@ -0,0 +1,5 @@
+-- Write your code here:
+SELECT YEAR(CURRENT_DATE) AS year,
+       MONTH(CURRENT_DATE) AS month,
+       DAY(CURRENT_DATE) AS day,
+       DAYOFMONTH(CURRENT_DATE) AS day_of_month;
