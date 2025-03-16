a86c99d8c3b2bc1c71f7e43a1f045bd71ff212fa
Marvelbad <badra.aqua@gmail.com>
2025-03-16 19:01:12 +0300

    add Solution SQL 0350

--- 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0350/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0350/query.sql
@@ -1,12 +1,10 @@
 -- Write your code here:
-WITH tempTable AS (SELECT AVG(grossed) AS averageGrossed
-                   FROM films),
-     tempTable2 AS (SELECT AVG(year_released) AS averageYear
-                    FROM films)
+WITH tempTable AS (SELECT AVG(grossed) AS averageGrossed FROM films),
+     tempTable2 (averageYear) AS (SELECT AVG(year_released) FROM films)
 SELECT title, genre, year_released, grossed
 FROM films,
      tempTable,
      tempTable2
 WHERE grossed > tempTable.averageGrossed
   AND year_released > tempTable2.averageYear
-GROUP BY year_released, genre, title, grossed;
+GROUP BY year_released;
