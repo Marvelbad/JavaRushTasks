c45356d03b2f464c966c99b3c142fb280de9c961
Marvelbad <badra.aqua@gmail.com>
2025-03-17 19:54:40 +0300

    add Solution SQL 0439

--- 3.JavaMultithreading/src/com/javarush/task/sql/task04/task0439/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task04/task0439/query.sql
@@ -1,6 +1,6 @@
 -- Write your code here:
-SELECT name        AS orogonal_name,
-       TRIM(name)  AS trimmed_name,
-       LTRIM(name) AS left_trimmed_name,
-       RTRIM(name) AS right_trimmed_name
+SELECT employee.name        AS orogonal_name,
+       TRIM(employee.name)  AS trimmed_name,
+       LTRIM(employee.name) AS left_trimmed_name,
+       RTRIM(employee.name) AS right_trimmed_name
 FROM employee;
