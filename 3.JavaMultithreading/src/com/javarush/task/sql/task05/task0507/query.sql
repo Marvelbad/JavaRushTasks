bbd77a46a064f8e6272918c2405959f09847caf6
Marvelbad <badra.aqua@gmail.com>
2025-03-18 16:36:55 +0300

    add Solution SQL 0507

--- 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0507/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0507/query.sql
@@ -1,1 +1,8 @@
 -- Write your code here:
+UPDATE part
+SET part.name = 'Technic, Steering Wheel Hub 3 Pin Round'
+WHERE part.number = 92909;
+
+UPDATE part
+SET part.name = 'Minifigure, Headgear Helmet SW Darth Vader Type 2 Top'
+WHERE part.number = 19916;
