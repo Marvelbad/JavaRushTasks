82e9b99b925ffd82ac6525d1327679369591900f
Marvelbad <badra.aqua@gmail.com>
2025-03-19 18:17:43 +0300

    add Solution SQL 0528

--- 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0528/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0528/query.sql
@@ -1,1 +1,6 @@
 -- Write your code here:
+SELECT DISTINCT publisher.name
+FROM book
+JOIN author ON author.id = book.author_id
+JOIN publisher ON book.publisher_id = publisher.id
+WHERE author.full_name = 'Mark Twain';
