83d0b4acdf6fc9afef3bb7edb78045911a833066
Marvelbad <badra.aqua@gmail.com>
2025-03-19 21:43:09 +0300

    add Solution SQL 0607

--- 3.JavaMultithreading/src/com/javarush/task/sql/task06/task0607/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task06/task0607/query.sql
@@ -1,1 +1,15 @@
 -- Write your code here:
+ALTER TABLE cars
+    ADD COLUMN `brand`       VARCHAR(50) NOT NULL,
+    ADD COLUMN `model`       VARCHAR(50) NOT NULL,
+    ADD COLUMN `year`        INT         NOT NULL,
+    ADD COLUMN `engine`      VARCHAR(50) NOT NULL,
+    ADD COLUMN `consumption` FLOAT       NOT NULL;
+
+INSERT INTO cars (cars.brand, cars.model, cars.year, cars.engine, cars.consumption)
+VALUES ('Lexus', 'IS', 2017, 'petrol', 7.5),
+       ('Volvo', 'XC90', 2019, 'diesel', 8.5),
+       ('Volkswagen', 'Golf', 2020, 'petrol', 8),
+       ('Toyota', 'Corolla', 2015, 'petrol', 10),
+       ('BMW', '5', 2005, 'petrol', 15.5),
+       ('Ford', 'Transit', 2010, 'diesel', 9);
