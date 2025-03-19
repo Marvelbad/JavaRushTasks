6df6801805e2889f0ebfa8bc8d93b8fa855f110b
Marvelbad <badra.aqua@gmail.com>
2025-03-19 18:58:51 +0300

    add Solution SQL 0530

--- 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0530/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0530/query.sql
@@ -1,1 +1,6 @@
 -- Write your code here:
+SELECT object.name,
+       YEAR(discovery_date)  AS d_year,
+       MONTHNAME(discovery_date) AS d_month,
+       DAYNAME(discovery_date)   AS d_day
+FROM object;
