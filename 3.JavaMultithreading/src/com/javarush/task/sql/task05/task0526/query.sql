953d3ee964424ea86a152f5166fceb00453509e4
Marvelbad <badra.aqua@gmail.com>
2025-03-19 17:48:48 +0300

    add Solution SQL 0526

--- 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0526/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0526/query.sql
@@ -1,1 +1,9 @@
 -- Write your code here:
+SELECT book.publication_year,
+       book.isbn,
+       book.title,
+       author.full_name,
+       publisher.name
+FROM book
+JOIN author ON book.author_id = author.id
+JOIN publisher ON book.publisher_id = publisher.id;
