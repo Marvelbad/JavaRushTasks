96f5237927fc4b3591de36876b54f86bf720d38b
Marvelbad <badra.aqua@gmail.com>
2025-03-19 19:59:36 +0300

    add Solution SQL 0604

--- 3.JavaMultithreading/src/com/javarush/task/sql/task06/task0604/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task06/task0604/query.sql
@@ -1,1 +1,10 @@
 -- Write your code here:
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users
+(
+    `user_id`         INT AUTO_INCREMENT,
+    `first_name` VARCHAR(255),
+    `last_name`  VARCHAR(255),
+    PRIMARY KEY (`user_id`)
+);
