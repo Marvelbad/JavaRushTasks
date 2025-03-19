3913696b202089d7cd97c25029f058384c129896
Marvelbad <badra.aqua@gmail.com>
2025-03-19 20:13:03 +0300

    add Solution SQL 0605

--- 3.JavaMultithreading/src/com/javarush/task/sql/task06/task0605/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task06/task0605/query.sql
@@ -1,1 +1,10 @@
 -- Write your code here:
+CREATE TABLE users
+(
+    `user_id`    INT AUTO_INCREMENT,
+    `first_name` VARCHAR(255)              NOT NULL,
+    `last_name`  VARCHAR(255)              NOT NULL,
+    `date`       DATE DEFAULT CURRENT_DATE NOT NULL,
+    `weight`     FLOAT(10)                 NULL,
+    PRIMARY KEY (user_id)
+);
