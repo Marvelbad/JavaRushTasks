e76707cbab8862696a83350057b97ddaf02b8e3e
Marvelbad <badra.aqua@gmail.com>
2025-03-20 21:26:25 +0300

    add Solution SQL 0632

--- 3.JavaMultithreading/src/com/javarush/task/sql/task06/task0632/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task06/task0632/query.sql
@@ -1,1 +1,6 @@
 -- Write your code here:
+ALTER TABLE event
+    ADD CONSTRAINT fk_event_user
+        FOREIGN KEY (user_id) REFERENCES users (id)
+            ON UPDATE CASCADE
+            ON DELETE SET NULL;
