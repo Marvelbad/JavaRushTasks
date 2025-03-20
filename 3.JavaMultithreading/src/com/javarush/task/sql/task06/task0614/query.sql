52a8de91bcb1933a47afd892e40f08b956eb67dc
Marvelbad <badra.aqua@gmail.com>
2025-03-20 17:28:18 +0300

    add Solution SQL 0614

--- 3.JavaMultithreading/src/com/javarush/task/sql/task06/task0614/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task06/task0614/query.sql
@@ -1,1 +1,10 @@
 -- Write your code here:
+UPDATE employee
+SET salary = salary + 1000
+WHERE employee_id IN (
+    SELECT e.employee_id
+    FROM employee AS e
+    LEFT JOIN task AS t ON e.employee_id = t.employee_id
+    AND t.exp_date = '2022-10-01'
+    WHERE t.employee IS NULL
+    );
