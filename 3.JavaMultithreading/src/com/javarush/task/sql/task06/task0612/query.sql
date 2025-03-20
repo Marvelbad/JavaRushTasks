75490df12a15f4f43827b595dcc8252729b26bb7
Marvelbad <badra.aqua@gmail.com>
2025-03-20 17:05:12 +0300

    add Solution SQL 0612

--- 3.JavaMultithreading/src/com/javarush/task/sql/task06/task0612/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task06/task0612/query.sql
@@ -1,1 +1,12 @@
 -- Write your code here:
+UPDATE employee
+SET salary   = 5000,
+    position = 'fullstack developer'
+WHERE position LIKE '%developer%';
+
+
+
+# SELECT position, salary
+# FROM employee
+# WHERE position LIKE '%developer%'
+# SET salary = 5000 AND position = 'fullstack developer';
