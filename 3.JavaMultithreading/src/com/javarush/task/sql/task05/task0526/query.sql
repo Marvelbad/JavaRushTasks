1b23b00c439ad7ffddbf580ea2c0c0ffdfcf3825
Marvelbad <badra.aqua@gmail.com>
2025-03-21 19:45:08 +0300

    Refactored 0526

--- 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0526/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0526/query.sql
@@ -3,7 +3,7 @@ SELECT book.publication_year,
        book.isbn,
        book.title,
        author.full_name,
-       publisher.name
+       library.publisher.name
 FROM book
 JOIN author ON book.author_id = author.id
-JOIN publisher ON book.publisher_id = publisher.id;
+JOIN library.publisher ON book.publisher_id = publisher.id;
