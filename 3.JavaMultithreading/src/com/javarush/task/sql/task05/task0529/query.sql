46506f954615257c8006c6e3549ac5375ca47476
Marvelbad <badra.aqua@gmail.com>
2025-03-21 19:52:30 +0300

    Refactored 0529

--- 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0529/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0529/query.sql
@@ -1,6 +1,6 @@
 -- Write your code here:
-SELECT author.full_name, COUNT(DISTINCT publisher.id) AS publishers
+SELECT library.author.full_name, COUNT(DISTINCT library.publisher.id) AS publishers
 FROM book
-         LEFT JOIN author ON book.author_id = author.id
-         LEFT JOIN publisher ON publisher.id = book.publisher_id
+         LEFT JOIN library.author ON book.author_id = author.id
+         LEFT JOIN library.publisher ON publisher.id = book.publisher_id
 GROUP BY author.id;
