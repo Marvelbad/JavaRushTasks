0b3f60b55fc778f5d71541c851dc69e3682ed3a5
Marvelbad <badra.aqua@gmail.com>
2025-03-21 17:24:29 +0300

    Refactored 0503

--- 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0503/init_data.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0503/init_data.sql
@@ -1,11 +1,14 @@
+CREATE DATABASE IF NOT EXISTS lego;
+
+USE lego;
+
 DROP TABLE IF EXISTS lego_set;
 
 CREATE TABLE lego_set
 (
-    `id`        INT          NOT NULL AUTO_INCREMENT,
-    `number`    INT          NOT NULL,
-    `name`      VARCHAR(100) NOT NULL,
-    `released`  INT          NOT NULL,
-    `inventory` INT          NOT NULL,
-    PRIMARY KEY (`id`)
+    `id`        int(11)      NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `number`    int(11)      NOT NULL,
+    `name`      varchar(100) NOT NULL,
+    `released`  int(11)      NOT NULL,
+    `inventory` int(11)      NOT NULL
 );
