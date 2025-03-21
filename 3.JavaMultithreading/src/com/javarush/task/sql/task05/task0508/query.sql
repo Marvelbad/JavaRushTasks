257b090b918c329bc584619179b2d3ca52769f2d
Marvelbad <badra.aqua@gmail.com>
2025-03-21 19:04:52 +0300

    Refactored 0508

--- 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0508/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0508/query.sql
@@ -1,12 +1,6 @@
 -- Write your code here:
-INSERT INTO author (id, first_name, last_name, full_name)
-VALUES
-    (10, 'John', 'Tolkien', 'J. R. R. Tolkien'),
-    (11, 'Friedrich', 'Nietzsche', 'Friedrich Nietzsche'),
-    (12, 'Aldous', 'Huxley', 'Aldous Huxley');
-
-UPDATE author SET author.full_name = 'Clive Staples Lewis' WHERE full_name = 'Lewis C. S. Lewis';
-UPDATE author SET author.full_name = 'John Ronald Reuel Tolkien' WHERE full_name = 'J. R. R. Tolkien';
-UPDATE author SET author.full_name = 'Friedrich Wilhelm Nietzsche' WHERE full_name = 'Friedrich Nietzsche';
-UPDATE author SET author.full_name = 'Stephen Edwin King' WHERE full_name = 'Stephen King Clive';
-UPDATE author SET author.full_name = 'Aldous Leonard Huxley' WHERE full_name = 'Aldous Huxley';
+UPDATE library.author SET author.full_name = 'Clive Staples Lewis' WHERE full_name = 'C. S. Lewis';
+UPDATE library.author SET author.full_name = 'John Ronald Reuel Tolkien' WHERE full_name = 'J. R. R. Tolkien';
+UPDATE library.author SET author.full_name = 'Friedrich Wilhelm Nietzsche' WHERE full_name = 'Friedrich Nietzsche';
+UPDATE library.author SET author.full_name = 'Stephen Edwin King' WHERE full_name = 'Stephen King Clive';
+UPDATE library.author SET author.full_name = 'Aldous Leonard Huxley' WHERE full_name = 'Aldous Huxley';
