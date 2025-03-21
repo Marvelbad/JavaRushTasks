9b93893f2286d9a2586dcebbd716a18550bd7684
Marvelbad <badra.aqua@gmail.com>
2025-03-21 18:25:49 +0300

    Refactored 0504

--- 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0504/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0504/query.sql
@@ -1,11 +1,10 @@
 -- Write your code here:
-INSERT INTO author (first_name, last_name, full_name)
-VALUES ( 'Joanne', 'Rowling', 'J.K. Joanne Rowling'),
-       ( 'Stephen', 'King', 'Stephen King Clive'),
-       ( 'Lewis', 'C.S.', 'Lewis C. S. Lewis'),
+INSERT INTO library.author (first_name, last_name, full_name)
+VALUES ( 'Joanne', 'Rowling', 'J.K. Rowling'),
+       ( 'Stephen', 'King', 'Stephen King'),
+       ( 'Clive', 'Lewis', 'C.S. Lewis'),
        ( 'Hajime', 'Isayama', 'Hajime Isayama'),
        ( 'Edgar', 'Burroughs', 'Edgar Burroughs'),
-       ( 'Edgar', 'Rice Burroughs', 'Edgar Rice Burroughs'),
-       ( 'Lewis', 'Carroll', 'Lewis Carroll'),
+       ( 'Lewis', 'Carrol', 'Lewis Carrol'),
        ( 'Astrid', 'Lindgren', 'Astrid Lindgren'),
        ( 'Richard', 'Scarry', 'Richard Scarry');
