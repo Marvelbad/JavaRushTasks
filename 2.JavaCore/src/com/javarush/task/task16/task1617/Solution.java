c4999f2124c6e471c79185a45411db75f4f28b84
Marvelbad <badra.aqua@gmail.com>
2025-03-31 20:15:46 +0300

    task 1617

--- 2.JavaCore/src/com/javarush/task/task16/task1617/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1617/Solution.java
@@ -5,11 +5,12 @@ package com.javarush.task.task16.task1617;
 */
 
 public class Solution {
-    public static volatile int numSeconds = 3;
+    public static volatile int numSeconds = 4;
 
     public static void main(String[] args) throws InterruptedException {
         RacingClock clock = new RacingClock();
-        //add your code here - добавь код тут
+        Thread.sleep(3500);
+        clock.interrupt();
     }
 
     public static class RacingClock extends Thread {
@@ -18,7 +19,16 @@ public class Solution {
         }
 
         public void run() {
-            //add your code here - добавь код тут
+            try {
+                while (numSeconds > 0) {
+                    System.out.print(numSeconds + " ");
+                    Thread.sleep(1000);
+                    numSeconds--;
+                }
+                System.out.println("Марш!");
+            } catch (InterruptedException e) {
+                System.out.println("Прервано!");
+            }
         }
     }
 }
