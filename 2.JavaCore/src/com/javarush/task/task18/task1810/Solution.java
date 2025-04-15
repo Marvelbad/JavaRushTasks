489fbb6f39b4e1783075166112c511c2e3346846
Marvelbad <badra.aqua@gmail.com>
2025-04-15 20:15:41 +0300

    task 1810

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1810/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task18.task1810;
+
+import java.io.*;
+
+/* 
+DownloadException
+*/
+
+public class Solution {
+    public static void main(String[] args) throws DownloadException, IOException {
+        try (
+                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            while (true) {
+                String filename = reader.readLine();
+                try (FileInputStream fis = new FileInputStream(filename)) {
+                    if (fis.available() < 1000) {
+                        throw new DownloadException();
+                    }
+                }
+            }
+        }
+    }
+
+    public static class DownloadException extends Exception {
+    }
+}
+
+
+
