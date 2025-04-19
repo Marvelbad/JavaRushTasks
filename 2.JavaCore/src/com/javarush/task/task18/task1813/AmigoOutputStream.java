fa28209acd76f573d764cd5f6a25e4acefc868fe
Marvelbad <badra.aqua@gmail.com>
2025-04-19 11:58:58 +0300

    1813

--- 2.JavaCore/src/com/javarush/task/task18/task1813/AmigoOutputStream.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1813/AmigoOutputStream.java
@@ -9,17 +9,43 @@ AmigoOutputStream
 */
 
 public class AmigoOutputStream extends FileOutputStream {
-    public FileOutputStream outputStream;
-
-    public AmigoOutputStream(FileOutputStream outputStream) {
-        super();
-        this.outputStream = outputStream;
-    }
-
     public static String fileName = "C:/tmp/result.txt";
+
+    FileOutputStream fileOutputStream;
+
+    public AmigoOutputStream(FileOutputStream fileOutputStream) throws FileNotFoundException {
+        super(fileName);
+        this.fileOutputStream = fileOutputStream;
+    }
 
     public static void main(String[] args) throws FileNotFoundException {
         new AmigoOutputStream(new FileOutputStream(fileName));
     }
 
+    @Override
+    public void write(int b) throws IOException {
+        fileOutputStream.write(b);
+    }
+
+    @Override
+    public void write(byte[] b) throws IOException {
+        fileOutputStream.write(b);
+    }
+
+    @Override
+    public void write(byte[] b, int off, int len) throws IOException {
+        fileOutputStream.write(b, off, len);
+    }
+
+    @Override
+    public void flush() throws IOException {
+        fileOutputStream.flush();
+    }
+
+    @Override
+    public void close() throws IOException {
+        flush();
+        write("JavaRush © All rights reserved.".getBytes());
+        fileOutputStream.close();
+    }
 }
