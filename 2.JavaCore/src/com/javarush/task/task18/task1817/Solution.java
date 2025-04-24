d52049e2edf4544f0e5b5ad0dca7ee9935dc8b41
Marvelbad <badra.aqua@gmail.com>
2025-04-24 18:35:25 +0300

    1817

--- 2.JavaCore/src/com/javarush/task/task18/task1817/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1817/Solution.java
@@ -1,6 +1,8 @@
 package com.javarush.task.task18.task1817;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 /* 
 Пробелы
@@ -11,22 +13,28 @@ public class Solution {
         File path = new File(args[0]);
 
         try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
-            {
-                String line;
-                StringBuilder builder = new StringBuilder();
-                while ((line = reader.readLine()) != null) {
-                    builder.append(line);
-                }
-                String text = builder.toString();
-                int charCount = text.length();
-                int allSpaces = text.replaceAll("[^ ]", "").length();
-                double percentage = (double) allSpaces / charCount * 100;
-                double rounded = Math.round(percentage * 100.0) / 100.0;
 
-                System.out.println(rounded);
+            String line;
+            StringBuilder builder = new StringBuilder();
+            while ((line = reader.readLine()) != null) {
+                builder.append(line);
             }
+            String text = builder.toString();
+            int charCount = text.length();
+            int allSpaces = text.replaceAll("[^ ]", "").length();
+            double percentage = (double) allSpaces / charCount * 100;
+            double rounded = Math.round(percentage * 100.0) / 100.0;
+
+            System.out.println(rounded);
+
         } catch (FileNotFoundException e) {
             System.out.println("Error");
         }
+    }
+
+    public static void main1(String[] args) throws IOException {
+        String string = Files.readString(Path.of(args[0]));
+        System.out.printf("%.2f",
+                (double) string.replaceAll("[^ ]", "").length() / string.length() * 100);
     }
 }
