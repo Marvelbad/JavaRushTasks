1d83848a53bddd3f673f03fc7e50cacae965f564
Marvelbad <badra.aqua@gmail.com>
2025-04-24 20:14:21 +0300

    1818 Refactored

--- 2.JavaCore/src/com/javarush/task/task18/task1818/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1818/Solution.java
@@ -32,4 +32,32 @@ public class Solution {
             System.out.println(e.getMessage());
         }
     }
+
+    /**
+     *
+     * Плохое решение из-за производительности.
+     * Постоянный и частый доступ к диску
+     */
+
+    public static void main2(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName1 = reader.readLine();
+        String fileName2 = reader.readLine();
+        String fileName3 = reader.readLine();
+
+        try (FileOutputStream fileOutputStream = new FileOutputStream(fileName1);
+             FileInputStream inputStream1 = new FileInputStream(fileName2);
+             FileInputStream inputStream2 = new FileInputStream(fileName3)) {
+            while (inputStream1.available() > 0) {
+                fileOutputStream.write(inputStream1.read());
+            }
+            while (inputStream2.available() > 0) {
+                fileOutputStream.write(inputStream2.read());
+            }
+
+
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
