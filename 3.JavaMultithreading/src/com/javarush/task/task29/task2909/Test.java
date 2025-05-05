f148bbd09e9bcdfc657cdc66e982f9f733ae59bc
Marvelbad <badra.aqua@gmail.com>
2025-05-06 00:14:07 +0300

    2909 refactoring stage 2

---
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/Test.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task29.task2909;
+
+import com.javarush.task.task29.task2909.human.Human;
+import com.javarush.task.task29.task2909.human.Soldier;
+
+public class Test {
+    public static void main(String[] args) {
+        new Human("Men", 22);
+        new Soldier("Sol", 25);
+        System.out.printf("OK");
+    }
+}
--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Human.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Human.java
@@ -14,8 +14,6 @@ public class Human {
 
     protected int[] size;
 
-    protected boolean isSoldier;
-
     public static final int FIRST = 1;
     public static final int SECOND = 2;
     public static final int THIRD = 3;
@@ -39,18 +37,9 @@ public class Human {
     }
 
 
-    public void setBloodGroup(int code) {
-        bloodGroup = code;
-    }
-
-    public int getBloodGroup() {
-        return bloodGroup;
-    }
-
-    public Human(boolean isSoldier) {
-        this.isSoldier = isSoldier;
-        this.id = nextId;
-        nextId++;
+    public Human(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 
     public int getAge() {
@@ -73,13 +62,6 @@ public class Human {
         return course;
     }
 
-    public void live() {
-        if (isSoldier)
-            fight();
-    }
-
-    public void fight() {
-    }
 
     public int getId() {
         return id;
