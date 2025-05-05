7239749588a9fa244222622bba18dfdcad90bfc8
Marvelbad <badra.aqua@gmail.com>
2025-05-05 18:31:00 +0300

    1821

--- 2.JavaCore/src/com/javarush/task/task18/task1821/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1821/Solution.java
@@ -1,10 +1,11 @@
 package com.javarush.task.task18.task1821;
 
 import java.io.*;
-import java.nio.Buffer;
-import java.util.HashMap;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 /* 
 Встречаемость символов
@@ -16,13 +17,17 @@ public class Solution {
         try (BufferedInputStream inputStream = new BufferedInputStream(new FileInputStream(args[0]))) {
             int b;
             while ((b = inputStream.read()) != -1) {
-                char c = (char) b;
-                map.put(c, map.getOrDefault(c, 0) + 1);
+                char character = (char) b;
+//                map.put(c, map.getOrDefault(c, 0) + 1);
+                map.merge(character, 1, Integer::sum);
             }
             for (Map.Entry entry : map.entrySet()) {
                 System.out.println(entry.getKey() + " " + entry.getValue());
             }
         }
+
+        Files.readString(Path.of(args[0])).chars().boxed()
+                .collect(Collectors.toMap(aChar -> (char) (aChar.intValue()), aChar -> 1, Integer::sum, TreeMap::new))
+                .forEach((key, value) -> System.out.println(key + " " + value));
     }
 }
-
