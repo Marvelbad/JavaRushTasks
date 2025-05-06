aad50f8d51d9bd629b4ae5626c53403bb551e0b8
Marvelbad <badra.aqua@gmail.com>
2025-05-06 21:26:12 +0300

    2909 refactoring stage 7

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Student.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Student.java
@@ -40,21 +40,17 @@ public class Student extends UniversityPerson {
     }
 
 
-    public void incAverageGradeBy01() {
-        averageGrade += 0.1;
-    }
-
-    public void incAverageGradeBy02() {
-        averageGrade += 0.2;
+    public void incAverageGrade(double delta) {
+        this.averageGrade += delta;
     }
 
 
-    public void setBeginningOfSession(int day, int month, int year) {
-        beginningOfSession = new Date(year, month, day);
+    public void setBeginningOfSession(Date date) {
+        beginningOfSession = date;
     }
 
-    public void setEndOfSession(int day, int month, int year) {
-        endOfSession = new Date(year, month, day);
+    public void setEndOfSession(Date date) {
+        endOfSession = date;
     }
 
     public double getAverageGrade() {
