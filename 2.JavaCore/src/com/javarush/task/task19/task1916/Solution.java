1dec55c11ea1a9a5131e6a12a13c9c26def4366c
Marvelbad <badra.aqua@gmail.com>
2025-05-19 21:30:31 +0300

    1916 Refactored

--- 2.JavaCore/src/com/javarush/task/task19/task1916/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1916/Solution.java
@@ -14,66 +14,65 @@ import java.util.List;
 public class Solution {
     public static List<LineItem> lines = new ArrayList<LineItem>();
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
-        String file1Lines = console.readLine();
-        String file2Lines = console.readLine();
+        String fileName1 = console.readLine();
+        String fileName2 = console.readLine();
         console.close();
 
         List<String> originalLines = new ArrayList<>();
         List<String> updatedLines = new ArrayList<>();
 
-        try (BufferedReader reader1 = new BufferedReader(new FileReader(file1Lines));
-             BufferedReader reader2 = new BufferedReader(new FileReader(file2Lines))
-
-        ) {
-            String originalLine;
-            while ((originalLine = reader1.readLine()) != null) {
-                originalLines.add(originalLine);
+        try (BufferedReader reader1 = new BufferedReader(new FileReader(fileName1));
+             BufferedReader reader2 = new BufferedReader(new FileReader(fileName2))) {
+            String line;
+            while ((line = reader1.readLine()) != null) {
+                originalLines.add(line);
+            }
+            while ((line = reader2.readLine()) != null) {
+                updatedLines.add(line);
             }
 
-            String updatedLine;
-            while ((updatedLine = reader2.readLine()) != null) {
-                updatedLines.add(updatedLine);
+            while (!originalLines.isEmpty() && !updatedLines.isEmpty()) {
+                if (originalLines.get(0).equals(updatedLines.get(0))) {
+                    lines.add(new LineItem(Type.SAME, originalLines.get(0)));
+                    originalLines.remove(0);
+                    updatedLines.remove(0);
+                } else if (updatedLines.size() > 1 && originalLines.get(0).equals(updatedLines.get(1))) {
+                    lines.add(new LineItem(Type.ADDED, updatedLines.get(0)));
+                    updatedLines.remove(0);
+                } else if (originalLines.size() > 1 && originalLines.get(1).equals(updatedLines.get(0))) {
+                    lines.add(new LineItem(Type.REMOVED, originalLines.get(0)));
+                    originalLines.remove(0);
+                }
             }
 
-            int i = 0;
-            int j = 0;
-
-            while (i < originalLines.size() && j < updatedLines.size()) {
-                if (originalLines.get(i).equals(updatedLines.get(j))) {
-                    lines.add(new LineItem(Type.SAME, originalLines.get(i)));
-                    i++;
-                    j++;
-
-                } else if ((j + 1) < updatedLines.size() && originalLines.get(i).equals(updatedLines.get(j + 1))) {
-                    lines.add(new LineItem(Type.ADDED, updatedLines.get(j)));
-                } else if ((i + 1) < originalLines.size() && originalLines.get(i + 1).equals(updatedLines.get(j))) {
-                    lines.add(new LineItem(Type.REMOVED, originalLines.get(i)));
-                    i++;
-                }   //ПЕРЕПИСАТЬ УДАЛЕНИЕМ!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            while (!originalLines.isEmpty()) {
+                lines.add(new LineItem(Type.REMOVED, originalLines.get(0)));
+                originalLines.remove(0);
             }
-
-            while (i < originalLines.size()) {
-                lines.add(new )
+            while (!updatedLines.isEmpty()) {
+                lines.add(new LineItem(Type.ADDED, updatedLines.get(0)));
+                updatedLines.remove(0);
             }
         }
     }
+}
 
 
-    public static enum Type {
-        ADDED,        //добавлена новая строка
-        REMOVED,      //удалена строка
-        SAME          //без изменений
+public static enum Type {
+    ADDED,        //добавлена новая строка
+    REMOVED,      //удалена строка
+    SAME          //без изменений
+}
+
+public static class LineItem {
+    public Type type;
+    public String line;
+
+    public LineItem(Type type, String line) {
+        this.type = type;
+        this.line = line;
     }
-
-    public static class LineItem {
-        public Type type;
-        public String line;
-
-        public LineItem(Type type, String line) {
-            this.type = type;
-            this.line = line;
-        }
-    }
+}
 }
