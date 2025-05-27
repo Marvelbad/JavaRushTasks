baddbb2f0916ca5398c05c7861ab5ceeb2da3c23
Marvelbad <badra.aqua@gmail.com>
2025-05-27 20:08:30 +0300

    1925

--- 2.JavaCore/src/com/javarush/task/task19/task1925/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1925/Solution.java
@@ -1,17 +1,29 @@
 package com.javarush.task.task19.task1925;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.List;
 
 /* 
 Длинные слова
 */
 
 public class Solution {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+             BufferedWriter writer = new BufferedWriter(new FileWriter(args[1]));
+        ) {
+            String line;
+            List<String> words = new ArrayList<>();
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(" ");
+                for (String word : parts) {
+                    if (word.length() > 6) {
+                        words.add(word);
+                    }
+                }
+            }
+            writer.write(String.join(",", words));
+        }
     }
 }
