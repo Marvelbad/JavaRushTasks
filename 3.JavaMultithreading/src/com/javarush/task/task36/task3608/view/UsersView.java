80d0a288bd7cdc898b84b87d6864ffc434c7e5de
Marvelbad <badra.aqua@gmail.com>
2025-05-31 13:58:13 +0300

    3608

--- 3.JavaMultithreading/src/com/javarush/task/task36/task3608/view/UsersView.java
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/view/UsersView.java
@@ -24,4 +24,9 @@ public class UsersView implements View {
     public void fireEventShowAllUsers() {
         controller.onShowAllUsers();
     }
+
+    public void fireEventShowDeletedUsers() {
+        controller.onShowAllDeletedUsers();
+    }
+
 }
