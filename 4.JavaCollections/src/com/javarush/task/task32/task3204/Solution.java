5c5332e0c430c4dbb2d1577bc7f394ca9ee4e222
Marvelbad <badra.aqua@gmail.com>
2025-06-02 14:26:40 +0300

    3204

---
+++ 4.JavaCollections/src/com/javarush/task/task32/task3204/Solution.java
@@ -0,0 +1,50 @@
+package com.javarush.task.task32.task3204;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Random;
+
+
+/* 
+Генератор паролей
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        ByteArrayOutputStream password = getPassword();
+        System.out.println(password.toString());
+    }
+
+    public static ByteArrayOutputStream getPassword() {
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        String digits = "0123456789";
+
+        String template = alphabet + alphabet.toUpperCase() + digits;
+        char[] password = new char[8];
+
+        Random random = new Random();
+
+
+        String string = "";
+        while (!string.matches(".*\\d.*") ||
+                !string.matches(".*\\p{Ll}.*") ||
+                !string.matches(".*\\p{Lu}.*")) {
+
+            for (int i = 0; i < password.length; i++) {
+                password[i] = template.charAt(random.nextInt(template.length()));
+            }
+            string = new String(password);
+        }
+
+
+        try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+            outputStream.write(string.getBytes());
+            return outputStream;
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+}
