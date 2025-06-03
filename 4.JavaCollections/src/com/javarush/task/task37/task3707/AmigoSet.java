4fb1fad70e98f850077e7617d8e143b056bf88fa
Marvelbad <badra.aqua@gmail.com>
2025-06-03 17:14:43 +0300

    3707

--- 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
@@ -1,77 +1,18 @@
 package com.javarush.task.task37.task3707;
 
-import java.io.*;
-import java.util.*;
+import java.io.Serializable;
+import java.util.AbstractSet;
+import java.util.Iterator;
+import java.util.Set;
 
 public class AmigoSet<E> extends AbstractSet<E> implements Serializable, Cloneable, Set<E> {
-
-    private static final Object PRESENT = new Object();
-    private transient HashMap<E, Object> map;
-
-    public AmigoSet() {
-        map = new HashMap<>();
-    }
-
-    public AmigoSet(Collection<? extends E> collection) {
-        map = new HashMap<>(Math.max(16, (int) (collection.size() / 0.75f) + 1));
-        addAll(collection);
-    }
-
-    @Override
-    public boolean add(E e) {
-        return map.put(e, PRESENT) == null;
-    }
-
     @Override
     public Iterator<E> iterator() {
-        return map.keySet().iterator();
+        return null;
     }
 
     @Override
     public int size() {
-        return map.size();
+        return 0;
     }
-
-    @Override
-    public boolean isEmpty() {
-        return map.isEmpty();
-    }
-
-    @Override
-    public boolean contains(Object o) {
-        return map.containsKey(o);
-    }
-
-    @Override
-    public void clear() {
-        map.clear();
-    }
-
-    @Override
-    public boolean remove(Object o) {
-        return map.remove(o) == PRESENT;
-    }
-
-    @Override
-    @SuppressWarnings("unchecked")
-    public Object clone() {
-        try {
-            AmigoSet<E> clone = (AmigoSet<E>) super.clone();
-            clone.map = (HashMap<E, Object>) map.clone();
-            return clone;
-        } catch (Exception e) {
-            throw new InternalError(e);
-        }
-    }
-
-    private void writeObject(ObjectOutputStream output) {
-        output.defaultWriteObject();
-        HashMapReflectionHelper.callHiddenMethod(map, "capacity");
-
-    }
-
-    private void readObject(ObjectInputStream input) {
-
-    }
-
 }
