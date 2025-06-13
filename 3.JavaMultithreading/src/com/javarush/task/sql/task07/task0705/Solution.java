ede318afae9a65e3213688aafaefa9f091568d8d
Marvelbad <badra.aqua@gmail.com>
2025-06-13 22:22:20 +0300

    0705 JDBC

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task07/task0705/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.sql.task07.task0705;
+
+import java.sql.*;
+
+/* 
+task0705
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        try (Connection connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/test", "root", "root");
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery("SELECT MIN(age) AS min_age FROM employee");
+        ) {
+            while (resultSet.next()) {
+                int age = resultSet.getInt("min_age");
+                System.out.println(age);
+            }
+        }
+    }
+}
