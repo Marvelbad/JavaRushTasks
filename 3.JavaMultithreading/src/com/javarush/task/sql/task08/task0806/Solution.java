5d28db4a6a00c2dfb8b00ba6f5bda14faeb3a4d6
Marvelbad <badra.aqua@gmail.com>
2025-06-16 23:21:22 +0300

    0806

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task08/task0806/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.sql.task08.task0806;
+
+import java.sql.*;
+import java.util.HashSet;
+import java.util.Set;
+
+/* 
+task0806
+*/
+
+public class Solution {
+
+    public static Set<Employee> employees = new HashSet<>();
+
+    public static void main(String[] args) throws Exception {
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "root");
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery("SELECT * FROM employee")
+        ) {
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                int age = resultSet.getInt("age");
+                String smth = resultSet.getString("smth");
+
+                employees.add(new Employee(id,name, age, smth));
+            }
+        }
+
+
+        employees.forEach(System.out::println);
+    }
+}
