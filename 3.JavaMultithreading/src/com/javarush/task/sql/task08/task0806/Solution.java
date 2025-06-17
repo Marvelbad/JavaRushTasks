1c6ab20c523db9bb2d83a45339e6cc5d7513db5b
Marvelbad <badra.aqua@gmail.com>
2025-06-17 21:43:39 +0300

    0806

--- 3.JavaMultithreading/src/com/javarush/task/sql/task08/task0806/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task08/task0806/Solution.java
@@ -15,7 +15,7 @@ public class Solution {
     public static void main(String[] args) throws Exception {
         try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "root");
              Statement statement = connection.createStatement();
-             ResultSet resultSet = statement.executeQuery("SELECT * FROM employee")
+             ResultSet resultSet = statement.executeQuery("SELECT name, age, smth FROM employee")
         ) {
             while (resultSet.next()) {
                 int id = resultSet.getInt("id");
