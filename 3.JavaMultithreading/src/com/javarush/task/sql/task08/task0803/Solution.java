76f12114a21fd89c1cbc5db166f0c56bb208fc83
Marvelbad <badra.aqua@gmail.com>
2025-06-17 21:43:20 +0300

    0803

--- 3.JavaMultithreading/src/com/javarush/task/sql/task08/task0803/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task08/task0803/Solution.java
@@ -17,25 +17,13 @@ public class Solution {
         String smth = "Senior pomidor";
         String sql = "insert into employee (name, age, smth) values (?, ?, ?)";
 
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-
-        try {
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "root");
-            preparedStatement = connection.prepareStatement(sql);
+        try (Connection connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/test", "root", "root");
+             PreparedStatement preparedStatement = connection.prepareStatement(sql);) {
             preparedStatement.setString(1, name);
             preparedStatement.setInt(2, age);
             preparedStatement.setString(3, smth);
             preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (preparedStatement != null) preparedStatement.close();
-                if (connection != null) connection.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
         }
     }
 }
