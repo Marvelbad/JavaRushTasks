8f8a425e02d73af4b22bb63e9df16521fb1a8d39
Marvelbad <badra.aqua@gmail.com>
2025-06-19 13:58:55 +0300

    4306 ObjectUtils.compare/Students

---
+++ 3.JavaMultithreading/src/com/javarush/task/jdk13/task43/task4306/Solution.java
@@ -0,0 +1,55 @@
+package com.javarush.task.jdk13.task43.task4306;
+
+/* 
+В поиске ботана
+*/
+
+import org.apache.commons.lang3.ObjectUtils;
+
+import java.lang.reflect.Field;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Student studentOne = new Student("Joe", 10, 8, 7, 7, 5, 6, 9);
+        Student studentTwo = new Student("Jane", 8, 9, 5, 6, 7, 7, 8);
+
+        String result = compareStudentGrades(studentOne, studentTwo);
+        System.out.println(result);
+    }
+
+    public static String compareStudentGrades(Student studentOne, Student studentTwo) {
+        if (studentOne == null || studentTwo == null) {
+            return "Make sure there are no null objects";
+        }
+        int result = ObjectUtils.compare(
+                getTotalScore(studentOne),
+                getTotalScore(studentTwo));
+        if (result > 0)
+            return studentOne.getName() + " has a higher grades score";
+        if (result < 0)
+            return studentTwo.getName() + " has a higher grades score";
+        return "Student grades scores are equal";
+    }
+
+    //Мой отдельный метод для суммирования каждого студента
+    public static int getTotalScore(Student s) {
+        int sum = 0;
+        Field[] declaredFields = Student.class.getDeclaredFields();
+        for (Field field : declaredFields) {
+            field.setAccessible(true);
+            Object value = null;
+            try {
+                value = field.get(s);
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+            if (value instanceof Integer && !field.getName().equals("name")) {
+                sum += (Integer) value;
+            }
+        }
+        return sum;
+    }
+}
+
+
