83396c5ce255e1a692b21e53998501b97ead98bf
Marvelbad <badra.aqua@gmail.com>
2025-06-20 16:37:28 +0300

    4101 classAdaptor

---
+++ 3.JavaMultithreading/src/com/javarush/task/jdk13/task41/task4101/Microscope.java
@@ -0,0 +1,12 @@
+package com.javarush.task.jdk13.task41.task4101;
+
+public class Microscope {
+
+    public void enlarge(Object o) {
+        System.out.println("🔬");
+    }
+
+    public void bang() {
+        System.out.println("Microscope broken");
+    }
+}
