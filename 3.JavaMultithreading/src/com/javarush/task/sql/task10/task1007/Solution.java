e6ed63afac48c348e0564a479c22bc9fec6bd7b4
Marvelbad <badra.aqua@gmail.com>
2025-06-26 13:53:43 +0300

    1007 query.setParameter

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task10/task1007/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.sql.task10.task1007;
+
+import com.javarush.task.sql.task10.task1005.MySessionFactory;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+
+import java.util.Collections;
+import java.util.List;
+
+/* 
+task1007
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        getIn(List.of("hr", "dev", "qa")).forEach(System.out::println);
+    }
+
+    public static List<Employee> getIn(List<String> in) {
+        List<Employee> employees = null;
+        try (SessionFactory sessionFactory = MySessionFactory.getSessionFactory();
+             Session session = sessionFactory.openSession();
+        ) {
+            Query query = session.createQuery(
+                    "from Employee where smth in (:in) order by age");
+            query.setParameter("in", in);
+            employees = query.getResultList();
+        }
+        return employees;
+    }
+}
