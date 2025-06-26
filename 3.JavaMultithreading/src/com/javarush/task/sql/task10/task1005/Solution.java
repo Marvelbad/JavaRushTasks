797e9f5d72a8966652cd5347744041941472fee5
Marvelbad <badra.aqua@gmail.com>
2025-06-26 13:03:48 +0300

    1005 query.getResultList

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task10/task1005/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.sql.task10.task1005;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import com.javarush.task.sql.task10.task1005.entities.Book;
+
+import java.util.List;
+
+/* 
+task1005
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        List<Book> books = null;
+
+        try (SessionFactory sessionFactory = MySessionFactory.getSessionFactory();
+             Session session = sessionFactory.openSession();
+        ) {
+            Query query = session.createQuery("from Book where author.fullName = 'Mark Twain'" +
+                    "and publisher.name = 'Фолио'");
+            books = query.getResultList();
+        }
+        books.forEach(System.out::println);
+
+    }
+}
