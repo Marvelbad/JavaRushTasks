c1c285e95f463302fc6de524609b6d12705fbbc0
Marvelbad <badra.aqua@gmail.com>
2025-07-10 21:21:22 +0300

    3401 Fibonachi

--- 2.JavaCore/src/com/javarush/task/jdk13/task34/task3401/Solution.java
+++ 2.JavaCore/src/com/javarush/task/jdk13/task34/task3401/Solution.java
@@ -15,7 +15,9 @@ public class Solution {
     }
 
     public int fibonacci(int n) {
-        return 0;
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        return fibonacci(n - 1) + fibonacci(n - 2);
     }
 }
 
