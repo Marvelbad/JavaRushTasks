a76ffd7274a2405e535cfd08f70c12afdd052831
Marvelbad <badra.aqua@gmail.com>
2025-07-10 21:27:16 +0300

    3402 Factorial

--- 2.JavaCore/src/com/javarush/task/jdk13/task34/task3402/Solution.java
+++ 2.JavaCore/src/com/javarush/task/jdk13/task34/task3402/Solution.java
@@ -14,10 +14,7 @@ public class Solution {
     }
 
     public int factorial(int n) {
-        int result = 1;
-        for (int i = 1; i <= n; i++) {
-            result *= i;
-        }
-        return result;
+        if (n == 0 || n == 1) return 1;
+        return n * factorial(n -1);
     }
 }
