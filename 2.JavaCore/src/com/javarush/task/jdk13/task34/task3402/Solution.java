1f62bccba118272cefb872bbc4d6b3189c0a0f6a
Marvelbad <badra.aqua@gmail.com>
2025-07-15 19:41:54 +0300

    3402

--- 2.JavaCore/src/com/javarush/task/jdk13/task34/task3402/Solution.java
+++ 2.JavaCore/src/com/javarush/task/jdk13/task34/task3402/Solution.java
@@ -13,7 +13,7 @@ public class Solution {
         System.out.println(solution.factorial(1));     //1
     }
 
-    public int factorial(int n) {
+    public long factorial(int n) {
         if (n == 0 || n == 1) return 1;
         return n * factorial(n -1);
     }
