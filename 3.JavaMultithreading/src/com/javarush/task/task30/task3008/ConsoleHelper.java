7109f493e18a541fe6dd87f85d3d1ad43ed5f110
Marvelbad <badra.aqua@gmail.com>
2025-07-16 12:38:50 +0300

    3008 Chat

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
@@ -1,4 +1,35 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ConsoleHelper {
+    private static BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() {
+        String result = null;
+        try {
+            result = console.readLine();
+        } catch (IOException e) {
+            System.out.println("Произошла ошибка при попытке ввода текста. Попробуйте еще раз.");
+            result = readString();
+        }
+        return result;
+    }
+
+    public static int readInt() {
+        int result;
+        try {
+            result = Integer.parseInt(readString());
+        } catch (NumberFormatException e) {
+            System.out.println("Произошла ошибка при попытке ввода числа. Попробуйте еще раз.");
+            result = readInt();
+        }
+        return result;
+    }
 }
