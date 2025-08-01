5b83ae6ef436a8191224988aa353f575cf33861c
Marvelbad <badra.aqua@gmail.com>
2025-08-01 11:47:49 +0300

    3110

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -1,22 +1,24 @@
 package com.javarush.task.task31.task3110;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Archiver {
     public static void main(String[] args) throws Exception {
-        System.out.println("Введите полный путь архива:");
-        BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
-        String string = console.readLine();
-        Path path = Paths.get(string);
+        System.out.println("Введите полный путь архива: ");
+        BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
+        String zipFilePathInput = consoleReader.readLine();
+        Path path = Paths.get(zipFilePathInput);
 
         ZipFileManager zipFileManager = new ZipFileManager(path);
-        System.out.println("Теперь введите путь к файлу, который будем архивировать:)");
-        String fileToZipString = console.readLine();
-        Path fileToZip = Paths.get(fileToZipString);
-        zipFileManager.createZip(fileToZip);
+
+        System.out.println("Введите путь к файлу, который будем архивировать: ");
+        String sourceFilePathInput = consoleReader.readLine();
+        Path sourceFilePath = Paths.get(sourceFilePathInput);
+
+        zipFileManager.createZip(sourceFilePath);
     }
 }
