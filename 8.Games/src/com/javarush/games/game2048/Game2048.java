f862777f62115bd9f702453f30c034c973c8712f
Marvelbad <badra.aqua@gmail.com>
2025-08-19 15:02:02 +0300

    2048

--- 8.Games/src/com/javarush/games/game2048/Game2048.java
+++ 8.Games/src/com/javarush/games/game2048/Game2048.java
@@ -4,4 +4,10 @@ import com.javarush.engine.cell.*;
 import com.javarush.engine.cell.Game;
 
 public class Game2048  extends Game {
+    private static final int SIDE = 4;
+
+    @Override
+    public void initialize() {
+        setScreenSize(SIDE, SIDE);
+    }
 }
