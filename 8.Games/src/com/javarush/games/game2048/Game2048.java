feecd0f720613b4bbbdb24557f1e06f2448c9816
Marvelbad <badra.aqua@gmail.com>
2025-08-19 16:00:23 +0300

    2048

--- 8.Games/src/com/javarush/games/game2048/Game2048.java
+++ 8.Games/src/com/javarush/games/game2048/Game2048.java
@@ -3,11 +3,17 @@ package com.javarush.games.game2048;
 import com.javarush.engine.cell.*;
 import com.javarush.engine.cell.Game;
 
-public class Game2048  extends Game {
+public class Game2048 extends Game {
     private static final int SIDE = 4;
+    private int[][] gameField = new int[SIDE][SIDE];
 
     @Override
     public void initialize() {
         setScreenSize(SIDE, SIDE);
+        createGame();
+    }
+
+    private void createGame() {
+
     }
 }
