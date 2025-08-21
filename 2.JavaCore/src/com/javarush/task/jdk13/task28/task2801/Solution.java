9b60e72a795532c7e493cdffe20d675b476c8fdc
Marvelbad <badra.aqua@gmail.com>
2025-08-21 13:39:53 +0300

    2801

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task28/task2801/Solution.java
@@ -0,0 +1,65 @@
+package com.javarush.task.jdk13.task28.task2801;
+
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/* 
+Пишем свою ThreadFactory
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        class EmulatorThreadFactoryTask implements Runnable {
+            @Override
+            public void run() {
+                emulateThreadFactory();
+            }
+        }
+
+        ThreadGroup group = new ThreadGroup("firstGroup");
+        Thread thread = new Thread(group, new EmulatorThreadFactoryTask());
+
+        ThreadGroup group2 = new ThreadGroup("secondGroup");
+        Thread thread2 = new Thread(group2, new EmulatorThreadFactoryTask());
+
+        thread.start();
+        thread2.start();
+    }
+
+    private static void emulateThreadFactory() {
+        AmigoThreadFactory factory = new AmigoThreadFactory();
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName());
+            }
+        };
+        factory.newThread(r).start();
+        factory.newThread(r).start();
+        factory.newThread(r).start();
+    }
+
+    public static class AmigoThreadFactory implements ThreadFactory {
+        private static AtomicInteger factoryNumber = new AtomicInteger(1);
+        private AtomicInteger threadNumber = new AtomicInteger(1);
+        private ThreadGroup threadGroup;
+
+        private String format;
+
+        public AmigoThreadFactory() {
+
+            threadGroup = Thread.currentThread().getThreadGroup();
+            format = String.format("%s-pool-%s-thread-", threadGroup.getName(), factoryNumber.getAndIncrement());
+        }
+
+        @Override
+        public Thread newThread(Runnable runnable) {
+
+            Thread thread = new Thread(runnable, format + threadNumber.getAndIncrement());
+            thread.setDaemon(false);
+            thread.setPriority(Thread.NORM_PRIORITY);
+            return thread;
+        }
+    }
+}
