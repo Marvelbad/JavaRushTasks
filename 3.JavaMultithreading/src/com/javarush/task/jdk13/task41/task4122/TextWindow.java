5611d36b32b73fd31c6ea3a58ebc47543e57c8f2
Marvelbad <badra.aqua@gmail.com>
2025-09-25 11:53:52 +0300

    Task 4122

--- 3.JavaMultithreading/src/com/javarush/task/jdk13/task41/task4122/TextWindow.java
+++ 3.JavaMultithreading/src/com/javarush/task/jdk13/task41/task4122/TextWindow.java
@@ -2,22 +2,6 @@ package com.javarush.task.jdk13.task41.task4122;
 
 public class TextWindow {
 
-    public static class TextWindowState {
-        private final String text;
-
-        public TextWindowState(String text) {
-            this.text = text;
-        }
-    }
-
-    public TextWindowState save() {
-        return new TextWindowState(currentText.toString());
-    }
-
-    public void restore(TextWindowState state) {
-        this.currentText = new StringBuilder(state.text);
-    }
-
     private StringBuilder currentText;
 
     public TextWindow() {
@@ -30,5 +14,21 @@ public class TextWindow {
 
     public String getText() {
         return currentText.toString();
+    }
+
+    public TextWindowState save() {
+        return new TextWindowState(currentText.toString());
+    }
+
+    public void restore(TextWindowState state) {
+        this.currentText = new StringBuilder(state.text);
+    }
+
+    public static class TextWindowState {
+        private final String text;
+
+        public TextWindowState(String text) {
+            this.text = text;
+        }
     }
 }
